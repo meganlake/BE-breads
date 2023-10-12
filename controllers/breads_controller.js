2e717f8378f5790a001f5c7f662946cf03088d70
Megan Lake <meganlake95@gmail.com>
2023-10-11 21:09:56 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,13 +4,18 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('Index',
+        {
+        breads: Bread
+        }
+    )
+  // res.send(Bread)
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
 })
-
 
 module.exports = breads
