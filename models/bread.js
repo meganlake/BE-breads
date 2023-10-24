b03fc2d17ec7d82572f7fb36a436053282cba040
Megan Lake <meganlake95@gmail.com>
2023-10-23 20:42:47 -0700

    validation added

--- models/bread.js
+++ models/bread.js
@@ -10,7 +10,12 @@ const breadSchema = new Schema ({
     name: { type: String, required: true },
     hasGluten: { type: Boolean },
     image: { type: String, default: 'http://placehold.it/500x500.png' },
+    baker: {
+        type: String,
+        enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    }
 })
+
 
 //Bread Model
 const Bread = mongoose.model('Bread', breadSchema)
